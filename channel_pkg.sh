7e47c3a3f0eea283ac7997cd1779d711a07893f0
banxi1988 <banxi1988@gmail.com>
2014-12-13 10:23:02 +0800

    update pkg sh to auto pick apk

--- channel_pkg.sh
+++ channel_pkg.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 channels="umeng anzhi appchina baidu gfan google_play hiapk meizu qihoo360 tencent wandoujia xiaomi huawei lenovo oppo single web weibo weixin tieba offline"
 #channels="web"
+apk_path=release # dir which apk dist and mapping.zip dist
+module_name=youjia-app # app module name
+pre_package(){
+    if [ -d $apk_path ];then
+        rm -r $apk_path
+    fi
+    mkdir $apk_path
+}
 package(){
     for channel in $channels ; do
         echo $channel
         gradle aYZR -PchannelName=$channel
-        cp youjia-app/build/com.webapps.yuns_release-2.0.1-20100.apk $apk_path/$channel-2.0.1-20100.apk
+        apk=`ls -tU -1 $module_name/build/ | grep 'apk$' | head -1`
+        cp $module_name/build/$apk $apk_path/$channel-$apk
     done
 }
-apk_path=release
-if [ -d $apk_path ];then
-    rm -r $apk_path
-fi
-mkdir $apk_path
+
+post_package(){
+    zip -r $apk_path/mapping.zip $module_name/build/outputs/mapping
+}
+pre_package
 package
-zip -r mapping.zip youjia-app/build/outputs/mapping
+post_package
