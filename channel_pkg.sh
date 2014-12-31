14bc3fdacb268936b0b8d48d35951eb2ad31a536
zds <zengdesen@qq.com>
2014-12-31 16:08:07 +0800

    新的编译、打包配置

--- channel_pkg.sh
+++ channel_pkg.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-channels="umeng anzhi appchina baidu gfan google_play hiapk meizu qihoo360 tencent wandoujia xiaomi huawei lenovo oppo single web weibo weixin tieba offline"
-#channels="web"
+#channels="umeng anzhi appchina baidu gfan google_play hiapk meizu qihoo360 tencent wandoujia xiaomi huawei lenovo oppo single web weibo weixin tieba offline"
+channels="offline"
 package(){
     for channel in $channels ; do
         echo $channel
         gradle aYZR -PchannelName=$channel
-        cp youjia-app/build/com.webapps.yuns_release-2.0.1-20100.apk $apk_path/$channel-2.0.1-20100.apk
+        cp youjia-app/build/com.webapps.yuns_release-2.2.1-22100.apk $apk_path/$channel-2.2.1-22100.apk
     done
 }
 apk_path=release
