0c4506df35dc488910715ccc4aaa737c408a3d59
ZDS <zengdesen@qq.com>
2015-12-10 11:18:15 +0800

    更新打包脚本

--- channel_pkg.sh
+++ channel_pkg.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#channels="offline web umeng anzhi appchina baidu gfan google_play hiapk meizu qihoo360 tencent wandoujia xiaomi huawei lenovo oppo single weibo weixin tieba express appshare"
-#channels="offline-01 offline-02 offline-03 offline-04 offline-05 offline-06 offline-07 offline-08 offline-09 offline-10 offline-11 offline-12"
-channels="web"
+## 说明:offline=线下 web=网站 xycm=校园传媒 zjls=指尖零食 umeng=友盟 zskb=掌上课表 anzhi=安智市场 appchina=应用汇 baidu=百度的应用市场 gfan=机锋 meizu=魅族 qihoo360=360手机助手
+##     tencent=腾讯 wandoujia=豌豆荚 xiaomi=小米 huawei=华为 lenovo=联想 oppo=oppo应用商店 weibo=新浪微博 weixin=微信 tieba=百度贴吧 zyzs=众用助手
+#channels="offline web xycm zjls umeng zskb anzhi appchina baidu gfan meizu qihoo360 tencent wandoujia xiaomi huawei lenovo oppo weibo weixin tieba zyzs"
+#channels="offline web umeng zskb anzhi appchina baidu gfan google_play hiapk meizu qihoo360 tencent wandoujia xiaomi huawei lenovo oppo single weibo weixin tieba express appshare"
 #channels="offline"
+channels="test"
 apk_path=release # dir which apk dist and mapping.zip dist
 module_name=youjia-app # app module name
 pre_package(){
